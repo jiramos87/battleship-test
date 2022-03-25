252bd64f955ae665f486d06d8f89ca9567e794a6
jiramos87 <jiramos87@gmail.com>
2022-03-25 19:15:54 -0300

    board logic updated, random gen, visual details

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ let enemyBoard = JSON.parse(JSON.stringify(initialEnemyBoard))
 let playerBoard = JSON.parse(JSON.stringify(initialPlayerBoard))
 
 let shipLengths = [5,4,4,3,2]
+let shotHistory = []
 
 // 0 = empty
 // 1 = part of a ship
@@ -75,6 +76,10 @@ function App() {
   const [ running, setRunning ] = useState(false)
   const [ win, setWin ] = useState(false)
   const [ loose, setLoose ] = useState(false)
+  const [ place, setPlace ] = useState(false)
+  const [ positionDisplay, setPositionDisplay ] = useState('')
+
+ 
 
   const handleRestart = () => {
     setHitCount(0)
@@ -91,7 +96,11 @@ function App() {
     }
     
     randomEnemyBoardGen()
+    placeShips()
+    showPlayerShips()
+    shotHistory = []
     console.log('enemyBoard', enemyBoard)
+    
     playerBoard = initialPlayerBoard
     setRunning(true)
     setWin(false)
@@ -100,11 +109,13 @@ function App() {
     //setMessage(null)
   }
 
-  useEffect(() => {
-    putPlayerShips()
-  },[handleRestart])
 
-  const putPlayerShips = () => {
+
+  const placeShips = () => {
+    setPlace(true)
+  }
+
+  const showPlayerShips = () => {
     for(let j = 0; j < playerBoard.length ; j++) {
       for(let i = 0; i < playerBoard[j].length; i++) {
         if(playerBoard[j][i] == 1) {
@@ -115,6 +126,10 @@ function App() {
 
       }
     }
+  }
+
+  const handlePositionDisplay = (position) => {
+    setPositionDisplay(position)
   }
 
   const fireTorpedoAI = () => {
@@ -146,81 +161,100 @@ function App() {
 
   const fireTorpedo = (x,y) => {
     if(running) {
-      console.log(x,y)
-      console.log(enemyBoard[y-1][x-1])
-      
-      
-      
-      if(enemyBoard[y-1][x-1] == 1) {
-        setMessage('You hit a ship') 
-        console.log('You hit a ship')
-        
-        let id = `square-${x}-${y}`
-        let element = document.getElementById(id)
-        element.style.backgroundColor = 'red'
-        enemyBoard[y-1][x-1] = 2
-        setHitCount(hitCount + 1)
-        setTimeout(() => {
-          setMessage(null)
-        }, 1600)
-        
-      } else if  (enemyBoard[y-1][x-1] == 0){
-        setMessage('You missed the shot') 
-        
-        let id = `square-${x}-${y}`
-        let element = document.getElementById(id)
-        element.style.backgroundColor = 'white'
-        enemyBoard[y-1][x-1] = 3
-        setTimeout(() => {
-          setMessage(null)
-        }, 1600)
-      } 
-      console.log('end turn')
-      console.log('hitCount',hitCount)
-      setTimeout( () => {
-          fireTorpedoAI()
-        }, 900)
-      if(hitCount  == 17) {
-        setMessage('You won!')
-        setWin(true)
-        setRunning(false)
+      // console.log(x,y)
+      // console.log(enemyBoard[y-1][x-1])
+      if(shotHistory.includes(`${x}-${y}`)  === false) {
+        if(enemyBoard[y-1][x-1] == 1) {
+          setMessage('You hit a ship') 
+          console.log('You hit a ship')
+          setHitCount(hitCount + 1)
+          
+          let id = `square-${x}-${y}`
+          let element = document.getElementById(id)
+          element.style.backgroundColor = 'red'
+          enemyBoard[y-1][x-1] = 2
+          
+          setTimeout(() => {
+            setMessage(null)
+          }, 3000)
+          
+        } else if  (enemyBoard[y-1][x-1] == 0){
+          setMessage('You missed the shot') 
+          
+          let id = `square-${x}-${y}`
+          let element = document.getElementById(id)
+          element.style.backgroundColor = 'white'
+          enemyBoard[y-1][x-1] = 3
+          setTimeout(() => {
+            setMessage(null)
+          }, 3000)
+        } 
+        shotHistory.push(`${x}-${y}`)
+        // console.log('shotHistory',shotHistory)
+        console.log('end turn')
+        console.log('hitCount',hitCount)
+        setTimeout( () => {
+            fireTorpedoAI()
+          }, 900)
+        if(hitCount  >= 17) {
+          setMessage('You won!')
+          setWin(true)
+          setRunning(false)
 
+        }
+      
+        if(hitCountAI  >= 17) {
+          setMessage('You lost!')
+          setLoose(true)
+          setRunning(false)
+          
+        }  
+      } else {
+        setMessage(`you can't repeat shots`)
       }
-    
-      if(hitCountAI  == 17) {
-        setMessage('You lost!')
-        setLoose(true)
-        setRunning(false)
-        
-      }  
     }
 
   }
 
   const randomEnemyBoardGen = () => {
     console.log('shipLengths length:' , shipLengths.length)
-    for(let k = 0; k < shipLengths.length ; k++) {
-      let randomX = Math.floor(Math.random() * 10)
-      let randomY = Math.floor(Math.random() * 10)
-      let randomDir = Math.floor(Math.random() * 2)  // 0 for X direction, and 1 for Y direction
-      if (randomDir == 0) {
-        if (randomX + shipLengths[k] > 9) {
-          randomX = Math.floor(Math.random() * 5)
-        } 
-        console.log(`shipLength: ${shipLengths[k]}, randomX: ${randomX}, randomY: ${randomY}, randomDir: ${randomDir}`)
-        for(let i = randomX; i < randomX + shipLengths[k] ; i++) {
-          enemyBoard[randomY][i] = 1
+    let partsCount = 0
+    let invalid = true
+    while(invalid) {
+      for(let k = 0; k < shipLengths.length ; k++) {
+        let randomX = Math.floor(Math.random() * 10)
+        let randomY = Math.floor(Math.random() * 10)
+        let randomDir = Math.floor(Math.random() * 2)  // 0 for X direction, and 1 for Y direction
+        if (randomDir == 0) {
+          if (randomX + shipLengths[k] > 9) {
+            randomX = Math.floor(Math.random() * 5)
+          } 
+          console.log(`shipLength: ${shipLengths[k]}, randomX: ${randomX}, randomY: ${randomY}, randomDir: ${randomDir}`)
+          for(let i = randomX; i < randomX + shipLengths[k] ; i++) {
+            enemyBoard[randomY][i] = 1
+          }
+        } else if (randomDir == 1) {
+          if (randomY + shipLengths[k] > 9) {
+            randomY = Math.floor(Math.random() * 5)
+          }
+          console.log(`shipLength: ${shipLengths[k]}, randomX: ${randomX}, randomY: ${randomY}, randomDir: ${randomDir}`)
+          for(let j = randomY; j < randomY + shipLengths[k] ; j++) {
+            enemyBoard[j][randomX] = 1
+          }
         }
-      } else if (randomDir == 1) {
-        if (randomY + shipLengths[k] > 9) {
-          randomY = Math.floor(Math.random() * 5)
-        }
-        console.log(`shipLength: ${shipLengths[k]}, randomX: ${randomX}, randomY: ${randomY}, randomDir: ${randomDir}`)
-        for(let j = randomY; j < randomY + shipLengths[k] ; j++) {
-          enemyBoard[j][randomX] = 1
-        }
-      }
 
+        for(let y = 0; y < enemyBoard.length ; y++) {
+          for(let x = 0; x < enemyBoard[y].length ; x++){
+            if(enemyBoard[y][x] == 1) {
+              partsCount++ 
+            }
+          }
+        }
+        if(partsCount >= 17) {
+          invalid = false
+        }
+
+      }
     }
     
   }
@@ -230,20 +264,20 @@ function App() {
 
   return (
     <div className='main'>
-      <div className='col-8 d-flex flex-column align-items-center mx-3'>
+      <div className='col-9 d-flex flex-column align-items-center mx-2'>
         <div className='display-3 mb-3'>React Battleship</div>
         <div className='instructions'>
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 Instructions
           </button>
-          <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <div class="modal-title display-5" id="staticBackdropLabel"><h1 className='text-center'>Instructions:</h1></div>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-centered">
+              <div className="modal-content">
+                <div className="modal-header d-flex justify-content-center">
+                  <div className="modal-title display-5" id="staticBackdropLabel"><div>Instructions:</div></div>
+                  <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
+                <div className="modal-body">
                   
                   <p className='text-center'> Take shots on the Computer AI board until you sink all of your enemy's ships. The first player to make 18 correct shots wins!
                   </p>
@@ -254,41 +288,50 @@ function App() {
                     <div><div className='missed legend-square'></div> Missed shot</div>
                   </div>
                 </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Understood</button>
+                <div className="modal-footer start-button d-flex justify-content-center">
+                  <button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={handleRestart}>Start game</button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className='restart-btn btn bg-danger' onClick={handleRestart}>{ running ? 'restart' : ( win ? 'restart' : 'start')}</div>
+        <div className='restart-btn btn bg-danger' onClick={handleRestart}>{ running ? 'Restart' : ( win ? 'Restart' : 'Start')}</div>
           
-          <div className="game-screen w-100 d-flex flex-row justify-content-between">
-            <div className='player-board d-flex flex-column align-items-center justify-content-between'>
-              <div className='d-flex flex-column justify-content-center align-items-center'>
-                <div className='name-div display-5'>You</div>
-                <div className='count-div text-danger d-flex flex-column justify-content-center align-items-center border rounded'>score: {hitCount}</div>
-              </div>
-              <BoardAI/>
+        <div className="game-screen w-100 d-flex flex-row justify-content-between">
+          <div className='player-board d-flex flex-column align-items-center justify-content-between'>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+              <div className='name-div display-5'>You</div>
+              <div className='count-div text-danger d-flex flex-column justify-content-center align-items-center border rounded'>Score: {hitCount}</div>
             </div>
-            <div className='info-div w-25 d-flex flex-column justify-content-center align-items-center'>
-              
-              <div className='info-message mt-3 text-center'>{ running ? message : ( win ? 'You won!': '' )}</div>
-              
-            </div>
-            <div className='enemy-board d-flex flex-column align-items-center justify-content-between'>
-              <div className='d-flex flex-column justify-content-center align-items-center'>
-                <div className='name-div display-5'>Computer</div>
-                <div className='count-div text-danger d-flex flex-column justify-content-center align-items-center border rounded'>score: {hitCountAI}</div>
-              </div>
-              
-              <Board fireTorpedo={fireTorpedo}/>
-            </div>
+            <BoardAI/>
           </div>
+          <div className='info-div d-flex flex-column align-items-center'>
+            
+            <div className='info-message h-50 d-flex flex-column justify-content-center mt-3 text-center'>
+              { running ? message : ( win ?  
+                <div>
+                  <div className='display-3'>{win ? 'Victory!' : ''}</div>
+                  <div className='display-3'>{loose ? 'Defeat!' : ''}</div>
+                </div> 
+              : '')}
+              </div>
+            <div className='h-50 d-flex flex-column justify-content-center align-items-center mt-5'>
+            <h3>Shot position:</h3>
+              <div className='pos-display border rounded text-center'>  {positionDisplay}</div>
+            </div> 
         
-        <div className='display-3'>{win ? 'Victory!' : ''}</div>
-        <div className='display-3'>{loose ? 'Defeat!' : ''}</div>
+          </div>
+          <div className='enemy-board d-flex flex-column align-items-center justify-content-between'>
+            <div className='d-flex flex-column justify-content-center align-items-center'>
+              <div className='name-div display-5'>Computer</div>
+              <div className='count-div text-danger d-flex flex-column justify-content-center align-items-center border rounded'>Score: {hitCountAI}</div>
+            </div>
+            
+            <Board fireTorpedo={fireTorpedo} positionDisplay={handlePositionDisplay}/>
+          </div>
+        </div>
+        
+        
         
       </div>
     </div>
